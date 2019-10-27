c9843099aa73e011381c4924b65bed4f9e663419
misdake <misdake@gmail.com>
2019-10-27 11:09:55 +0800

    remove the step that closes Chrome.

--- capture.bat
+++ capture.bat
@@ -2,4 +2,3 @@ start "" /B "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user
 ping localhost -n 2 >NUL
 node index.js --delay=1000
 ping localhost -n 2 >NUL
-C:\Software\NirCmd\nircmd.exe win close ititle "Google Chrome"
